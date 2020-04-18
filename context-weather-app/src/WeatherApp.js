aa8044f26a6d2f3ada81fa3e7cfa53f124a798ee
Nilber Remon <nilberremon@gmail.com>
2020-04-18 11:35:10 -0400

    App React Router Implementation and Frontend Routes Built

--- context-weather-app/src/WeatherApp.js
+++ context-weather-app/src/WeatherApp.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
 import './App.css';
+
+import Home from './Components/Home'
+import WeatherProvider from './Providers/WeatherProvider'
+
 
 function App() {
   return (
-    <div className="App">
-      
-    
+    <div className="WeatherApp">
+            <NavBar />
+            <Switch>
+            <Route eaxct path={'/Home'} >
+                <Home />
+            </Route>
+            <Route exact path={'/Weather'}>
+              <WeatherProvider value={0}>
+                <WeatherZone/>
+              </WeatherProvider>
+            </Route>
+            <Route path={'/*'}> 
+                <ErrorPage />
+            </Route>
+            </Switch>
     </div>
-  );
+    )
 }
 
 export default App;
